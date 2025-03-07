106efd5d14ad96015e40ffa3c759f51cd0deffcc
emarveaux <elina.emx@gmail.com>
2025-03-07 17:04:20 +0100

    feat: overall design

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ class Footer extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-<footer class="d-flex flex-wrap justify-content-between align-items-center p-3 my-4 border-top">
+<footer class="d-flex flex-wrap justify-content-between align-items-center p-3 my-4 border-top" style="background-color: #5d98d0;>
         <div class="col-md-4 d-flex align-items-center">
             <span class="mb-3 mb-md-0 text-body-secondary">&copy; Elina Marveaux - 2025</span>
         </div>
